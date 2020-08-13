51b25753ac787c563b84bcfe845ac3ed6da0ceb4
Roberto Garcia <robertojesusgarcia@gmail.com>
2020-08-12 21:31:53 -0500

    feat: view with APIView

--- teachers/urls.py
+++ teachers/urls.py
@@ -2,9 +2,11 @@ from django.conf.urls import url
 from django.urls import path
 from rest_framework import routers
 
-from teachers.views import teachers, detail_teacher
+from teachers.views import teachers, detail_teacher, TeachersView, TeacherDetailView
 
 urlpatterns = [
     path('teachers/', teachers),
     path('teachers/<teacher_id>/', detail_teacher),
+    path('teachers_view/', TeachersView.as_view()),
+    path('teachers_view/<pk>/', TeacherDetailView.as_view()),
 ]
--- teachers/views.py
+++ teachers/views.py
@@ -1,8 +1,46 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from teachers.models import Teacher
 from teachers.serializers import TeacherSerializer
+
+
+class TeachersView(APIView):
+
+    def get(self, request):
+        teachers_data = Teacher.objects.all()
+        serialized = TeacherSerializer(teachers_data, many=True)
+        return Response(data=serialized.data)
+
+    def post(self, request):
+        teacher_data = {
+            'first_name': request.data['first_name'],
+            'last_name': request.data['last_name'],
+            'phone': request.data['phone']
+        }
+        Teacher.objects.create(**teacher_data)
+        return Response(status=201)
+
+
+class TeacherDetailView(APIView):
+
+    def get(self, request, pk):
+        teacher = Teacher.objects.get(id=pk)
+        serialized = TeacherSerializer(teacher)
+        return Response(data=serialized.data)
+
+    def put(self, request, pk):
+        teacher = Teacher.objects.get(id=pk)
+        for field in request.data:
+            print(f'Field {field}, value {request.data[field]}')
+            setattr(teacher, field, request.data[field])
+        teacher.save()
+        return Response(status=200)
+
+    def delete(self, request, pk):
+        Teacher.objects.get(id=pk).delete()
+        return Response(status=204)
 
 
 @api_view(['GET', 'POST'])
@@ -33,6 +71,7 @@ def detail_teacher(request, teacher_id):
 
     if request.method == 'PUT':
         teacher = Teacher.objects.get(id=teacher_id)
+        print(request.data)
         for field in request.data:
             print(f'Field {field}, value {request.data[field]}')
             setattr(teacher, field, request.data[field])
